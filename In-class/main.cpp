1c48074e6db89bc0280d0511baa2590bbe0736f7
Soilderboy <xxsoilderboy49@gmail.com>
2025-01-27 11:37:03 -0600

    inclass 1

--- In-class/main.cpp
+++ In-class/main.cpp
@@ -12,9 +12,8 @@ Your weekly pay: 550
 
 
 #include <iostream>
-
-
 using namespace std;
+using std::cout;
 int main()
 {
     double payRate;
@@ -28,26 +27,38 @@ int main()
     cin >> hours;
 
 //Input validation
-   while (//write your code here) {
+   while (hours < 0) {
         cout << "\nInvalid input. Hours cannot be negative" << endl;
         cout << "Enter a valid value: " ;
         cin >> hours;
 
     }
+ 
+    int regularHours;
+    int overtimeHours;
 
+//three if statements
+//checks hours and then calculates overtimeHours with a cap of 10 hours on it
+    if(hours> 50){
+        weeklyWages = (40 * payRate) + (10 * (1.5 * payRate));
+        regularHours = 40;
+        overtimeHours = 10;
+    } else if(hours > 40 && hours <= 50) {
+        weeklyWages = (40 * payRate) + ((hours - 40) * (1.5 * payRate));
+        regularHours = 40;
+        overtimeHours = hours - 40;
+    } else {
+        weeklyWages = hours * payRate;
+        regularHours = hours;
+        overtimeHours = 0;
+    }
     
-/*
-
-      Enter Code
-      
-   */
    
-   /*
    // This is how your output shoudl be. 
    //If you declare variables accordingly you should be able to get correct answers.
     cout << "\nRegular hours: " <<  regularHours;
-    cout << " Overtime hours: " <<   overtimeHours << endl;
+    cout << " Overtime hours: " <<  overtimeHours << endl;
     cout << "Your weekly pay: " << weeklyWages << endl;
-   */
 
+    return 0;
 }
