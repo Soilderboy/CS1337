19289d303c3d6125ddcbf257f79343362241430e
Soilderboy <xxsoilderboy49@gmail.com>
2025-02-17 21:00:40 -0600

    hw2 done

--- Assignments/HW2.cpp
+++ Assignments/HW2.cpp
@@ -150,6 +150,14 @@ void modifiedSortID(int array2[], double array1[], string array3[], int size)
    // Function body
 }
 */
+//Given function prototypes
+// void modifiedSortGPA(double array1[], int array2[], string array3[], int numElems);
+// void modifiedSortID(int array2[], double array1[], string array3[], int numElems);
+// //double randGPA();
+// int getUserChoice();
+// int linearSearchID(int arr[], int numElems, int ID, int &nIter);
+// int binarySearchID(int arr[], int numElems, int ID, int &nIter);
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -160,13 +168,13 @@ void modifiedSortID(int array2[], double array1[], string array3[], int size)
 using namespace std;
 
 
-const int NUM_ELMTS = 18;
+const int NUM_ELMTS = 10;
 
 // function prototypes
 int linearSearchID(int arr[], int numElems, int value, int &nIter);
 int binarySearchID(int arr[], int numElems, int value, int &nIter);
-void modifiedSortGPA(double array1[], int array2[], string array3[], int size);
-void modifiedSortID(double array1[], int array2[], string array3[], int size);
+void modifiedSortGPA(double array1[], int array2[], string array3[], int size); //array1 is GPA, array2 is netID, array3 is major
+void modifiedSortID(int array2[], double array1[], string array3[], int size); //array1 is netID, array2 is GPA, array3 is major
 
 int main(){
 
@@ -193,19 +201,79 @@ int main(){
     cout << "Enter file name: ";
     cin >> filename;
 
+    file.open(filename);
+
     if(!file){
         cout << "Could not open file\n";
         cout << "Exiting\n";
         return 1;
     }
+    int count = 0;
+    while(file >> netID[count] >> major[count] >> GPA[count]){
+        count++;
+        if(count >= NUM_ELMTS) break;
+    }
+    //count is going to be total elements in array
+    cout << "Original arrays:" << endl;
+    cout << "----------------" << endl;
 
-    for(int i = 0; i < NUM_ELMTS; i++){
-        file >> netID[i] >> major[i] >> GPA[i];
+    //display contents of all arrays
+    cout << "index" << setw(7) << "netID" << setw(7) << "major" << setw(7) << "GPA"
+    << setw(7) << "index" << setw(7) << "netID" << setw(7) << "major" << setw(7) << "GPA" << endl;
+    for(int i = 0; i < count; i++){
+        cout << setw(5) << i << setw(7) << netID[i] << setw(7) << major[i] << setw(7) << fixed << setprecision(2) << GPA[i];
+        if(i%2 == 1){ //print two records per line
+            cout << endl;
+        }
     }
 
+    //arrays sorted by GPA descending order
+    cout << "Arrays sorted by GPA:" << endl;
+    cout << "---------------------" << endl;
+    for(int i = 0; i < count; i++){
+        sortedGPAbyGPA[i] = GPA[i];
+        sortedNetIDbyGPA[i] = netID[i];
+        sortedMajorbyGPA[i] = major[i];
+    }
+    //call modifiedSortGPA
+    modifiedSortGPA(sortedGPAbyGPA, sortedNetIDbyGPA, sortedMajorbyGPA, count);
+
+    //display GPA sorted arrays
+    cout << "index" << setw(7) << "netID" << setw(7) << "major" << setw(7) << "GPA"
+    << setw(7) << "index" << setw(7) << "netID" << setw(7) << "major" << setw(7) << "GPA" << endl;
+    for(int i = 0; i < count; i++){
+        cout << setw(5) << i << setw(7) << sortedNetIDbyGPA[i] << setw(7) << sortedMajorbyGPA[i] << setw(7) << fixed << setprecision(2) << sortedGPAbyGPA[i];
+        if(i%2 == 1){ //print two records per line
+            cout << endl;
+        }
+    }
+
+    //copy netID to sortedNetIDbyID, copy GPA to sortedGPAbyID and copy major to sortedMajorbyID
+    for(int i = 0; i < count; i++){
+        sortedNetIDbyID[i] = netID[i];
+        sortedGPAbyID[i] = GPA[i];
+        sortedMajorbyID[i] = major[i];
+    }
+
+    //sort sortedNetIDbyID
+    modifiedSortID(sortedNetIDbyID, sortedGPAbyID, sortedMajorbyID, count);
+
+    cout << "Arrays sorted by netID:" << endl;
+    cout << "-----------------------" << endl;
+    
+    //display GPA sorted arrays
+    cout << "index" << setw(7) << "netID" << setw(7) << "major" << setw(7) << "GPA"
+    << setw(7) << "index" << setw(7) << "netID" << setw(7) << "major" << setw(7) << "GPA" << endl;
+    for(int i = 0; i < count; i++){
+        cout << setw(5) << i << setw(7) << sortedNetIDbyID[i] << setw(7) << sortedMajorbyID[i] << setw(7) << fixed << setprecision(2) << sortedGPAbyID[i];
+        if(i%2 == 1){ //print two records per line
+            cout << endl;
+        }
+    }
     //loop and display three choices
     int choice;
     do{
+
         cout << "***************" << endl;
         cout << "Menu of choices" << endl;
         cout << "***************" << endl;
@@ -222,17 +290,70 @@ int main(){
             cin >> n;
 
             //display top n students with descending GPA
+            if(n > NUM_ELMTS){
+                n = NUM_ELMTS;
+            }
 
-            //sort GPA array by GPA using modifiedSortGPA
+            cout << "Top " << n << " students are:" << endl;
+            cout << endl;
+
+            //display top n students
+            //GPA is sorted in descending order and we need to display it in ascending order
+            //so we start from the end of the array
+            for(int i = n-1; i >= 0; i--){
+                cout << "netID: " << sortedNetIDbyGPA[i] << ", " << "major: " << sortedMajorbyGPA[i] << ", "
+                 << "GPA: " << fixed << setprecision(2) << sortedGPAbyGPA[i] << endl;
+            }
+
+
+        }
+        else if(choice == 2){ //search on NetID
+            int ID;
+            cout << "Enter netID: ";
+            cin >> ID;
+
+            //linear search on original array
+            int nIterLinear = 0;
+            int indexLinear = linearSearchID(netID, count, ID, nIterLinear);
+
+            //binary search on sortedNetIDbyID
+            int nIterBinary = 0;
+            int indexBinary = binarySearchID(sortedNetIDbyID, count, ID, nIterBinary);
+
+            cout << "Result of linear search:" << endl;
+            cout << "------------------------" << endl;
+            if(indexLinear != -1){
+                cout << "Student found at index " << indexLinear << ", GPA is " << GPA[indexLinear] << endl;
+            }
+            else{
+                cout << "Student not found" << endl;
+            }
+            cout << "It took " << nIterLinear << " iterations" << endl;
+            cout << endl;
+            cout << "Result of binary search:" << endl;
+            cout << "------------------------" << endl;
+            if(indexBinary != -1){
+                cout << "Student found at index " << indexBinary << ", GPA is " << sortedGPAbyID[indexBinary] << endl;
+            }
+            else{
+                cout << "Student not found" << endl;
+            }
+            cout << "It took " << nIterBinary << " iterations" << endl;
+            cout << endl;
+            
 
         }
     } while(choice != 3); //exit loop
 
-
+    cout << "Exiting\n";
+    file.close();
+    return 0;
 }
 //linearSearchID means take the original array, numElems is max size, 
 //value is element we're looking for, nIter is number of itereations
+//return index of value
 int linearSearchID(int arr[], int numElems, int value, int &nIter){
+    nIter = 0;
     for(int i = 0; i < numElems; i++){
         nIter++;
         if(arr[i] == value){
@@ -243,6 +364,7 @@ int linearSearchID(int arr[], int numElems, int value, int &nIter){
 }
 //given a sorted array, find the value, return index
 int binarySearchID(int arr[], int numElems, int value, int &nIter){
+    nIter = 0;
     int low = 0;
     int high = numElems - 1;
     while(low <= high){
@@ -262,42 +384,57 @@ int binarySearchID(int arr[], int numElems, int value, int &nIter){
 
 }
 
-//swap functon for the sorting
+//uh, there's already a swap function in std so ignore this
+/*
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
 void swap(double &a, double &b){
     double temp = a;
     a = b;
     b = temp;
 }
+
+void swap(string &a, string &b){
+    string temp = a;
+    a = b;
+    b = temp;
+}
+*/
 //given an arra, sort it, maintain parallelism between arr1 arr2, and arr3
 //sort it by GPA let's do a selection sort
 //array1 is GPA, array2 is netID, array3 is major
-void modifiedSortGPA(double array1[], int array2[], string array3[], int size){
+//sort in ascending order
+void modifiedSortGPA(double GPA[], int netID[], string major[], int size){
     for(int i = 0; i < size - 1; i++){
         int minIndex = i;
         for(int j = i + 1; j < size; j++){
-            if(array1[j] < array1[minIndex]){
+            if(GPA[j] < GPA[minIndex]){
                 minIndex = j;
             }
         }
         //swap
-        swap(array1[i], array1[minIndex]);
-        swap(array2[i], array2[minIndex]);
-        swap(array3[i], array3[minIndex]);
+        swap(GPA[i], GPA[minIndex]);
+        swap(netID[i], netID[minIndex]);
+        swap(major[i], major[minIndex]);
     }
 }
 
 //sort by ID, using bubble sort - maintain parallelism like above
-//array1 is GPA, array2 is netID, array3 is major
+//array1 is netID, array2 is GPA, array3 is major
 //I swapped array1 and array2's data types from the function prototypes given
-void modifiedSortID(double array1[], int array2[], string array3[], int size){
+//sort in ascending order
+void modifiedSortID(int netID[], double GPA[], string major[], int size){
     for(int i = 0; i < size - 1; i++){
         //size - 1 - i might go overbound
         for(int j = 0; j < size - 1 - i; j++){
             //if current element is greater than the next, swap
-            if(array2[j] > array2[j + 1]){
-                swap(array1[j], array1[j + 1]);
-                swap(array2[j], array2[j + 1]);
-                swap(array3[j], array3[j + 1]);
+            if(netID[j] > netID[j + 1]){
+                swap(netID[j], netID[j + 1]);
+                swap(GPA[j], GPA[j + 1]);
+                swap(major[j], major[j + 1]);
             }
         }
     }
