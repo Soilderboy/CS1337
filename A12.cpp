74a0377295cd1c0993f1b841259e2ca07b0407c2
Soilderboy <xxsoilderboy49@gmail.com>
2025-03-26 14:10:26 -0500

    a12

--- A12.cpp
+++ A12.cpp
@@ -1,3 +1,17 @@
+/*Psuedocode:
+So I'm given a circle class
+
+I need to instantiate c1 a circle object
+Set the radius and print the data of it
+
+Then a pointer to c2 and set the radius through that
+Print the data through the pointer
+
+When defining functions in the class, I just do it like Circle::function() {}
+
+The const keyword after a function means the function can't change the object's data members - meaning read-only
+*/
+
 // This program demonstrates a simple class.
 #include <iostream>
 using namespace std;
@@ -18,18 +32,24 @@ class Circle
 
 
 // setRadius assigns a value to the radius member.
-
+void Circle::setRadius(double r){
+   radius = r;
+}
 
 // getRadius assigns a value to the radius member.
+double Circle::getRadius() const{
+   return radius;
+}
 
 // getDiameter 
+double Circle::getDiameter() const{
+   return radius * 2;
+}
 
 // getArea
-
-
-
-
-
+double Circle::getArea() const{
+   return PI * radius * radius;
+}
 
 int main()
 {
@@ -46,25 +66,36 @@ int main()
 
 
    // Store the radius of the circle in the c1 object.
+   if(rad < 0){ //input validation
+      cout << "Radius must be a positive value" << endl;
+      return -1;
+   }
+   c1.setRadius(rad);
+
  
 
    // Display the circle's data.
    cout << "\nHere is the circle's data:\n";
-   cout << "Radius: " <<  << endl;
-   cout << "Diameter: " << << endl;
-   cout << "Area: " <<  << endl;
+   cout << "Radius: " << c1.getRadius() << endl;
+   cout << "Diameter: " << c1.getDiameter() << endl;
+   cout << "Area: " << c1.getArea() << endl;
 
     Circle *c2 = new Circle;
     
 
    // Store the radius of the circle in the c2 object.
+   if(rad1 < 0){ //input validation
+      cout << "Radius must be a positive value" << endl;
+      return -1;
+   }
+   c2->setRadius(rad1);
    
 
    // Display the circle's data.
    cout << "\nHere is the circle's data through the pointer:\n";
-   cout << "Radius: " <<  << endl;
-   cout << "Diameter: " <<  << endl;
-   cout << "Area: " <<  << endl;
+   cout << "Radius: " << c2->getRadius() << endl;
+   cout << "Diameter: " << c2->getDiameter() << endl;
+   cout << "Area: " << c2->getArea() << endl;
 
    return 0;
 }
